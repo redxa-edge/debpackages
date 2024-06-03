4de8fa4bfb29a287697fd8bdf6d5c39a49ccccf1
Abhishek Singh <abhishek@edgeproc.ai>
2024-06-03 23:14:27 +0530

    added instruction to add repo and install packages

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # edge.repo
 demo apt repository for edge
+
+run the shell script to install any application present in the repo like:
+
+```bash
+sudo addRepo.sh -i <packagename>
+
+```
+
+you can get the shell script like:
+
+```bash
+wget https://raw.githubusercontent.com/redxa-edge/debpackages/main/addRepo.sh
+```
--- addRepo.sh
+++ addRepo.sh
@@ -2,38 +2,33 @@
 alreadyInstalled=false
 installation=false
 uninstallation=false
-PREFIX=/data/data/com.termux/files/usr
+PREFIX=/
 packages=()
 
-function validateUsr() {
-  pathOfCurrentDir=$(pwd)
-  if [[ $pathOfCurrentDir == *'com.termux'* ]]; then
-    :
-  else
-    printf "Your system is not compatible for this repository!\n"; exit 1
-  fi
-}
+
+
 function checkAlreadyInstalled() {
-  if [[ -f $PREFIX/etc/apt/sources.list.d/edge.repo.list ]]; then
+  if [[ -f $PREFIX/etc/apt/sources.list.d/edgerepo.list ]]; then
     alreadyInstalled=true
   else
     alreadyInstalled=false
   fi
 }
+
 function installRepository() {
   if [[ ! -d $PREFIX/etc/apt/sources.list.d ]]; then
     mkdir -p $PREFIX/etc/apt/sources.list.d
   fi
-  echo "deb [trusted=yes] https://redxa-edge.github.io/edge.repo/ edge main" >$PREFIX/etc/apt/sources.list.d/edge.repo.list
+  echo "deb [trusted=yes] https://redxa-edge.github.io/debpackages/ edge main" >$PREFIX/etc/apt/sources.list.d/edge.repo.list
   rm -rf public.key >/dev/null 2>&1
-  wget -q https://raw.githubusercontent.com/BHUTUU/edge.repo/main/public.key
+  wget -q https://raw.githubusercontent.com/redxa-edge/debpackages/main/public.key
   apt-key add public.key
-  cp -r $PREFIX/etc/apt/trusted.gpg~ $PREFIX/etc/apt/trusted.gpg.d/edge.gpg
+  cp -r $PREFIX/etc/apt/trusted.gpg $PREFIX/etc/apt/trusted.gpg.d/edge.gpg
   apt-get update -yq --silent
   printf "Installation successful\n"
 }
 function removeRepository() {
-  rm -rf $PREFIX/etc/apt/sources.list.d/edge.repo.list >/dev/null 2>&1
+  rm -rf $PREFIX/etc/apt/sources.list.d/edgerepo.list >/dev/null 2>&1
   printf "Removing successful!\n"
 }
 
@@ -52,7 +47,6 @@ while true; do
     break
   fi
 done
-validateUsr
 if [[ $installation == true ]]; then
   if [[ $uninstallation == true ]]; then
     printf "Dont use both of -i and -r argunment\n"
